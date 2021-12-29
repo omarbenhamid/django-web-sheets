738d01ebbfc13fae139bdbcdc538ba568d0fd2ab
Omar BENHAMID <contact@obenhamid.me>
2021-12-29 20:09:24 +0100

    Moving to specific module admin inline

--- tests/demo/admin.py
+++ tests/demo/admin.py
@@ -1,52 +1,16 @@
 from django.contrib import admin
 from .models import Book, Author, Category
-from django.contrib.admin.options import ModelAdmin, TabularInline,\
-    InlineModelAdmin
-from django.forms.forms import Form
-from django.forms.formsets import formset_factory, BaseFormSet
-from django.forms.models import BaseInlineFormSet
+from websheets.admin import WebSheetInlineModelAdmin
+from django.contrib.admin.options import ModelAdmin
 
 # Register your models here.
 admin.site.register(Book)
 admin.site.register(Category)
 
-class WebSheetForm(Form):
-    pass
 
-class WebSheetFormSet(BaseFormSet):
-    def __init__(self, **kwargs):
-        self.instance=kwargs.pop('instance')
-        self.queryset=kwargs.pop('queryset')
-        if 'save_as_new' in kwargs: 
-            kwargs.pop('save_as_new')
-        
-        BaseFormSet.__init__(self, **kwargs) #data, files, auto_id, prefix, initial, error_class, form_kwargs)
-    
-    def save(self):
-        self.new_objects=[]
-        self.changed_objects=[]
-        self.deleted_objects=[]
-    
-    @classmethod
-    def get_default_prefix(cls):
-        #TODO: models.inlineformset_factory look at jow it is done
-        return "TODO-deduce-me-from-fk-as"
-
-    def get_queryset(self):
-        return self.queryset.none()
-    
-class BookInline(InlineModelAdmin):
-    template="websheets/admin_inline.html"
+class BookInline(WebSheetInlineModelAdmin):
     model=Book
     
-    def get_css_width(self):
-        return "100%";
-    
-    def get_css_height(self):
-        return "10em"
-    
-    def get_formset(self, request, obj=None, **kwargs):
-        return formset_factory(WebSheetForm, WebSheetFormSet)
     
 @admin.register(Author)
 class AuthorAdmin(ModelAdmin):
--- websheets/admin.py
+++ websheets/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.contrib.admin.options import ModelAdmin, InlineModelAdmin
+from django.forms.forms import Form
+from django.forms.formsets import formset_factory, BaseFormSet
+from django.forms.models import BaseInlineFormSet
 
 # Register your models here.
+
+class WebSheetForm(Form):
+    pass
+
+class WebSheetFormSet(BaseFormSet):
+    def __init__(self, **kwargs):
+        self.instance=kwargs.pop('instance')
+        self.queryset=kwargs.pop('queryset')
+        if 'save_as_new' in kwargs: 
+            kwargs.pop('save_as_new')
+        
+        BaseFormSet.__init__(self, **kwargs) #data, files, auto_id, prefix, initial, error_class, form_kwargs)
+    
+    def save(self):
+        self.new_objects=[]
+        self.changed_objects=[]
+        self.deleted_objects=[]
+    
+    @classmethod
+    def get_default_prefix(cls):
+        #TODO: models.inlineformset_factory look at jow it is done
+        return "TODO-deduce-me-from-fk-as"
+
+    def get_queryset(self):
+        return self.queryset.none()
+    
+    
+class WebSheetInlineModelAdmin(InlineModelAdmin):
+    template="websheets/admin_inline.html"
+    
+    def get_css_height(self):
+        return "15em"
+    
+    def get_formset(self, request, obj=None, **kwargs):
+        FormSet=formset_factory(WebSheetForm, WebSheetFormSet)
+        FormSet.inline=self
+        return FormSet
+    
